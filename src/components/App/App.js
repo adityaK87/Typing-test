f864be49acec23edf57333aaa34ffb352c260ddd
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-10 10:31:37 +0530

    updated app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import LandingPage from '../LandingPage/LandingPage';
 import Navbar from '../Navbar/Navbar';
 import './App.css';
+import Footer from '../Footer/Footer';
 
 class App extends React.Component{
 
     render(){
       return(
         <div className='App'>
+        {/* NavBar */}
          <Navbar /> 
-         
+          
+        {/* LandingPage */}
+        <LandingPage />
+
+        {/* challenge-section */}
+        {/* Footer */}
+        <Footer />
         </div>
       );
     }
