8d693387c766f361bb9d40b1f34b75c5b89557ad
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-10 10:36:39 +0530

    landing page uploaded

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './LandingPage.css'
+import flash from '../logo&img/flash.png';
+import Typewriter from 'typewriter-effect';
+
+const LandingPage = () => {
+
+    return(
+        <div data-aos="fade-right" className="LandingPage-container ">
+        <div className="LandingPage-left">
+            <h1 className='Landing-header'>Can you type...</h1>
+            <div className="typewriter-container">
+     <Typewriter
+         options={{
+            strings: ['Fast?', 'Correct?','Quick?'],
+            autoStart: true,
+            loop: true,
+        }}
+    />
+            </div>
+
+        </div>
+        <div className="LandingPage-right">
+            <img src={flash} alt="" className='flash-img' data-aos="fade-left" />
+        </div>
+
+        </div>
+    );
+}
+
+export default LandingPage;
