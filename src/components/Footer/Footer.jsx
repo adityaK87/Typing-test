ff4f35dce141ed536332e1c4b84c9774cb1b181d
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-10 10:37:56 +0530

    Footer uploaded

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import  './Footer.css'
+const Footer = () => {
+    return(
+        <div className='footer-container'>
+            <a
+            target="_blank"
+            rel="noreferrer"
+            className='footer-link'
+             href="https://github.com/adityapal87/Typing-test">visit the code of this App </a>
+
+        </div>
+    );
+}
+export default Footer;
