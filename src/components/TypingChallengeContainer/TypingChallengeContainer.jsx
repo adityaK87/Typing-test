91eec7230b19b3efdacd187be0ea67f9d7365a38
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-13 11:10:55 +0530

    Add files via upload

---
+++ src/components/TypingChallengeContainer/TypingChallengeContainer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ChallengeDetailsCard from '../ChallengeDetailsCard/ChallengeDetailsCard';
+import TypingChallenge from '../TypingChallenge/TypingChallenge';
+import './TypingChallengeContainer.css'
+
+const TypingChallengeContainer = ({
+    selectedParagraph,  
+    words, 
+    characters,
+    wpm,
+    timeRemaining ,
+    timeStarted}) => {
+        return(
+            <div className="Typing-challenge-container">
+                    {/* details section */}
+                        <div className="details-container">
+
+                            {/* words typed */}
+                            <ChallengeDetailsCard cardName="Words" cardValue = {words}/>
+
+                            {/* character typed */}
+                            <ChallengeDetailsCard cardName="Characters" cardValue = {characters}/>
+
+                            {/* speed */}
+                            <ChallengeDetailsCard cardName="Speed" cardValue = {wpm}/>
+                      
+                        </div>
+                    {/* The real challenge */}
+                    <div className="typeWriter-container">
+                        <TypingChallenge
+                         timeStarted=  {timeStarted}
+                        timeRemaining={timeRemaining}                        
+                         selectedParagraph={selectedParagraph}/>
+                    </div>
+            </div>
+        );
+
+}
+export default TypingChallengeContainer;
