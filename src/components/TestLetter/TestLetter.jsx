c0ebc969ae00b308621338812e3d854c082e665e
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-14 11:13:53 +0530

    added TestLetter component to src fliles

---
+++ src/components/TestLetter/TestLetter.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './TestLetter.css'
+
+const TestLetter =  ({individualLetterInfo}) => { 
+    const {status} = individualLetterInfo;
+    // let statusClass; 
+
+
+    const statusClass = {
+        correct:"test-letter-correct",
+        incorrect:"test-letter-incorrect",
+        notAttempted:"test-letter-not-attempted"
+    }[status]
+
+    // if(status === "correct"){
+    //     statusClass = "test-letter-correct";
+    // } else if (status === "incorrect"){
+    //     statusClass="test-letter-incorrect";
+    // }else if(status === "not-attempted") {
+    //     statusClass = "test-letter-not-attempted";
+    // }
+
+    // console.log(individualLetter)
+    return(
+       <span className= {`test-letter ${statusClass}`}>
+            {individualLetterInfo.testLetter}
+       </span>
+    );
+}
+export default TestLetter;
