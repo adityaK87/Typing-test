c3a4cf738cc0d0b8beaefaa2dc342554d08164e9
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-15 11:10:00 +0530

    modify app.jsx

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,41 +6,134 @@ import Footer from '../Footer/Footer';
 import ChallengeSection from '../ChallengeSection/ChallengeSection';
 // import axios from 'axios';
 
-const paraURL="http://metaphorpsum.com/paragraphs/2/9"
+const paraURL="http://metaphorpsum.com/paragraphs/1/10"
  
-const totalTime = 40;
+const totalTime = 2;
+const DefaultState ={
+  selectedParagraph:"",
+  timeStarted:false,
+  timeRemaining: totalTime,
+  words:0,
+  characters:0,
+  wpm:0,
+  testInfo:[]
+} 
+
 class App extends React.Component{
 
-  state={
-    selectedParagraph:"Namaste India!!",
-    timeStarted:false,
-    timeRemaining: totalTime,
-    words:0,
-    characters:0,
-    wpm:0,
-    testInfo:[]
-}
+  state = DefaultState;
 
-  componentDidMount(){
-    // fetch(paraURL)
-    // .then((response)=>response.text())
-    // .then((data)=>{//console.log(data);
-    //   this.setState({selectedParagraph:data})
-    // });
 
-    const selectedParagraphArray = this.state.selectedParagraph.split("")
-    // console.log( "splitted para -",selectedParagraphArray)
-    const testInfo = selectedParagraphArray.map((selectedLetter)=>{
-      return {
-        testLetter:selectedLetter,
-        status:"notAttempted",
-      };
-    })
-    this.setState({testInfo})
+  fetchNewParagraph = () => { 
+ 
+    fetch(paraURL)
+    .then((response)=>response.text())
+    .then((data)=>{
+      //console.log(data);
+      this.setState({selectedParagraph:data})
+      const selectedParagraphArray = data.split("");
+      // console.log( "splitted para -",selectedParagraphArray)
+      const testInfo = selectedParagraphArray.map((selectedLetter)=>{
+        return {
+          testLetter:selectedLetter,
+          status:"notAttempted",
+        };
+      })
+      this.setState({
+        ...DefaultState,
+        testInfo,
+        selectedParagraph:data});
+    });
+
   }
 
+  componentDidMount (){
+ 
+    this.fetchNewParagraph();
+
+  }
+
+setTimer = () => {
+    this.setState({timeStarted:true})
+    const timer = setInterval(() => {
+      if( this.state.timeRemaining > 0){
+        // change the WPM 
+        const timeSpent = totalTime - this.state.timeRemaining;
+        const wpm = 
+        timeSpent > 0
+        ? (this.state.words/timeSpent) * totalTime 
+        : 0;
+
+        this.setState({
+          timeRemaining:this.state.timeRemaining - 1,
+          wpm:parseInt(wpm),
+        })
+      }else{
+        clearInterval(timer)
+      }
+      
+    }, 1000);
+}
+
+startAgain = () => this.fetchNewParagraph();
+
+
    handleUserInput = (inputValue) => {
-    console.log(inputValue) 
+    // console.log(inputValue) 
+    if(!this.state.timeStarted) this.setTimer()
+      /**
+         * 1. Handle the underflow case - all characters should be shown as not-attempted
+         * 2. Handle the overflow case - early exit
+         * 3. Handle the backspace case
+         *      - Mark the [index+1] element as notAttempted
+         *        (irrespective of whether the index is less than zero)
+         *      - But, don't forget to check for the overflow here
+         *        (index + 1 -> out of bound, when index === length-1)
+         * 4. Update the status in test info
+         *      1. Find out the last character in the inputValue and it's index
+         *      2. Check if the character at same index in testInfo (state) matches
+         *      3. Yes -> Correct
+         *         No  -> Incorrect (Mistake++)
+         * 5. Irrespective of the case, characters, words and wpm can be updated
+         */
+        const characters = inputValue.length; 
+        const words = inputValue.split(" ").length;
+        const index = characters-1
+        if(index < 0){
+          this.setState({
+             testInfo:[
+               { testLetter : this.state.testInfo[0].testLetter,
+                status : "notAttempted"
+               },
+               ...this.state.testInfo.slice(1),
+            ],
+            characters,
+            words
+          });
+          return; 
+        }
+
+        if(index >= this.state.selectedParagraph.length){
+          this.setState({characters ,words})
+          return ;
+        }
+        //make a copy of testInfo
+        const testInfo = this.state.testInfo;
+        if(!(index===this.state.selectedParagraph.length-1))
+            testInfo[index+1].status = "notAttempted"; 
+
+        // check for the correct typed letter
+        const isCorrect = inputValue[index]===testInfo[index].testLetter;
+
+        // update the testInfo
+        testInfo[index].status = isCorrect ? "correct" : "inCorrect";
+
+        // update the state
+        this.setState({
+          testInfo,
+          words,
+          characters
+   });
 
    }
 
@@ -76,7 +169,7 @@ class App extends React.Component{
          timeStarted = {this.state.timeStarted}
          testInfo={this.state.testInfo}
          onInputChange={this.handleUserInput}
-
+         startAgain={this.startAgain}
          />
 
 
