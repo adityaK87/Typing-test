30f3f6e945a6c32908a0ffca72a233596b759fd8
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-20 08:51:15 +0530

    modify navBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import './Navbar.css'
-import logo from '../logo& img/logo.png'
+import logo from '../logo&img/logo.png'
+import gitLogo from '../logo&img/gitLogo1.png'
 
 const Navbar=()=>{
     
     return(
         <div className='nav-container'>
             <div className="nav-left">
-                <img className="nav-logo"  src ={logo} alt="" />
+                <img className="nav-logo"  src ={logo} alt="logo" />
                 <p className="nav-logo-text"> <i>Typing Test</i></p>
             </div>
+            
             <div className="nav-right">
                 <a 
                 href="https://github.com/adityapal87"
                 target="_blank"
                 className="nav-git-link"
                 rel='noreferrer'
-                >Git-Link
+                >
+                <img src={gitLogo} alt="" width= "35px" />
                 </a>
             </div>
+            
         </div>
     );
 
