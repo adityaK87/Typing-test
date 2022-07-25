56d5419e39b5efca54a9bac9573152ead3893b51
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-25 09:16:52 +0530

    Update TryAgain.jsx

--- src/components/TryAgain/TryAgain.jsx
+++ src/components/TryAgain/TryAgain.jsx
@@ -1,34 +1,24 @@
 import React from 'react';
 import './TryAgain.css'
 
-const TryAgain = ({words,characters,wpm}) =>{
-    return(
+const TryAgain = ({ words, characters, wpm, startAgain }) => {
+
+    return (
         <div className="try-again-container">
             <h1>Test Results </h1>
             <div className="test-result">
-            <p>
-                <b>Characters: {characters}</b>
-            </p>
+                <p><b>Characters : {characters}</b></p>
+                <p><b>Words : {words}</b></p>
+                <p> <b> Speed : {wpm}wpm </b></p>
 
-            <p>
-                <b>Words: {words}</b>
-            </p>
-
-            <p>
-                <b> Speed: {wpm}wpm  </b>
-            </p>
-         
             </div>
-        <div>
-            <button className='start-again-button'>Re-try</button>
+            <div>
+                <button className='start-again-button' onClick={() => startAgain()}>Re-try</button>
             </div>
 
         </div>
-    );
-}
-
+    );}
 export default TryAgain;
 
 // This TryAgain.jsx component is child component of TestContainer.jsx
 // This TryAgain.jsx component is sibling component of TypingChallengeContainer.jsx
- 
